81ec51f3394ef99a297ed1fb080fa0afe81bbbb1
Fattigman <32386766+Fattigman@users.noreply.github.com>
2019-11-19 15:13:57 +0100

    Update nodes_insert.py

--- nodes_insert.py
+++ nodes_insert.py
@@ -1,7 +1,7 @@
 #inserts all nodenames from final file into the tabe nodes.
 import sqlite3
 
-def fetchnodenames(filename):#fetches all nodenames from given file
+def fetchnodenames(filename):
     nodelist = []
     with open(filename) as f:
         lines = filter(None, (line.rstrip() for line in f.readlines()))
@@ -9,16 +9,16 @@ def fetchnodenames(filename):#fetches all nodenames from given file
             if line.split("\t")[0]!="C1":
                 nodelist.append(line.split("\t")[8])
     return nodelist
-nodenames = fetchnodenames("a_ar_c_g_g3_n_p_s_t-top1-9999925.txt.final.h.txt")
-#connects to the database and inserts all the names into the nodes table
+nodenames = fetchnodenames("a_ar_c_g_g3_n_p_s_t-top1-9999925.txt.final.h.txt")'
+#connects to the database and creates a table node
 conn = sqlite3.connect("ignv.db")
 try:
     c = conn.cursor()
 except Error as e:
     print e
-c.execute('''CREATE TABLE IF NOT EXISTS nodes(
-node_id integer primary key,
-node_name text not null
+c.execute('''CREATE TABLE IF NOT EXISTS node(
+id integer primary key,
+name text not null
 );
 ''')
 for item in nodenames:
