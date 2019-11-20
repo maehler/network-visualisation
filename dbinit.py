b97fbbfda6907221fcb649eaff3890cf2ef09796
Fattigman <32386766+Fattigman@users.noreply.github.com>
2019-11-20 12:44:47 +0100

    Update dbinit.py

--- dbinit.py
+++ dbinit.py
@@ -7,23 +7,23 @@ except Error as e:
     print e
 #Creates a node table
 c.executescript('''CREATE TABLE IF NOT EXISTS node(
-id integer primary key,
-name text not null
+    id integer primary key,
+    name text not null
 );
 CREATE TABLE IF NOT EXISTS edge(
-node1 integer not null,
-node2 integer not null,
-directionality integer not null,
-FOREIGN KEY(node1) REFERENCES nodes(node_id)
-FOREIGN KEY(node2) REFERENCES nodes(node_id)
+    node1 integer not null,
+    node2 integer not null,
+    directionality integer not null,
+    FOREIGN KEY(node1) REFERENCES nodes(node_id)
+    FOREIGN KEY(node2) REFERENCES nodes(node_id)
 );
 CREATE TABLE IF NOT EXISTS module(
-name text not null,
-id primary key
+    name text not null,
+    id primary key
 );
 CREATE TABLE IF NOT EXISTS pathway(
-name text not null,
-id primary key
+    name text not null,
+    id primary key
 );
 ''')
 
