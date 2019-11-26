0672ec78aa3e66f148ea5b48f295621ae224d01d
Fattigman <32386766+Fattigman@users.noreply.github.com>
2019-11-26 13:15:25 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var cors = require('corser')
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var apiRouter = require('./routes/api');
+var usersRouter = require('./routes/users');
+var jsRouter = require('./routes/js');
+var sqlite3 = require("sqlite3").verbose();
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', apiRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/js', jsRouter);
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
+module.exports = app;
