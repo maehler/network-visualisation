1dd3c6cb38f39ace204e55e3fb1ea0bdd5234773
Jacob Karlström <jacob.karlstrom@gmail.com>
2019-12-09 15:54:52 +0100

    changed to function instead of class

--- server/data.js
+++ server/data.js
@@ -1,42 +1,29 @@
 
-var sqlite3 = require("sqlite3").verbose();
-var path = require('path');
+const db = require("better-sqlite3")("../sqlserver/ignv.db");
 var util = require("util");
-var flag = 0;
-const dbPath = path.resolve("/home/jacob/Dokument/Växtprojekt/sqlserver", "ignv.db")
-console.log(dbPath)
-//Inititializes the database connection
-function test(){
-  var db = new sqlite3.Database(dbPath,(err) => {
-    if (err){
-      return console.error(err.message);
+
+// Inititializes the database connection
+var getNetwork = function(){
+    edgeinsert = 'select * from edge;';
+    nodeinsert = 'SELECT n.id, m.m_id, n.name FROM node n inner join node_module m on m.n_id = n.id;';
+    apidata = [];
+    for(const edge of db.prepare(edgeinsert).iterate()){
+      apidata.push({
+        "group":"edges",
+        "data":{
+          "id":("e"+edge.id),
+          "source":edge.node1,
+          "target":edge.node2}});
     }
-    console.log("Connected yo");
-    });
-    let nodesql = 'SELECT n.id, m.m_id, n.name FROM node n inner join node_module m on m.n_id = n.id;';
-    let edgesql = 'SELECT * FROM edge;';
-    data =[];
-    db.serialize(() => {
-      db.each(nodesql, [], (err,row)=>{
-        if(err){
-          console.error(err.message);
-        }
-        data.push({"group":"nodes", "data":{"id":row.id.toString(), "name":row.name.toString(), "module":row.m_id.toString()}});
-      })
-      .each(edgesql, [], (err, row)=>{
-        if(err){
-          console.error(err.message);
-        }
-        data.push({"group":"edges", "data":{"id":("e"+row.id.toString()), "source":row.node1.toString(), "target":row.node2.toString()}});
-      })
-    });
-      db.close((err) =>{
-      if(err){
-        return console.error(err.message);
-      }
-      console.log('Close the database connection.');
-      console.log(data)
-      return data;
-  })
-}
-test()
+    for(const node of db.prepare(nodeinsert).iterate()){
+      apidata.push(
+        {"group":"nodes",
+        "data":{
+          "id":node.id,
+          "name":node.name,
+          "module":node.m_id}});
+    }
+    return apidata;
+  }
+
+module.exports.getNetwork = getNetwork;
