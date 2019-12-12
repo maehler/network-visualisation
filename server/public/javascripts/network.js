62c94917af635f273b31016115cc3e3c06bfca84
Jacob Karlström <jacob.karlstrom@gmail.com>
2019-12-12 16:12:04 +0100

    Added name popup when selecting node

--- server/public/javascripts/network.js
+++ server/public/javascripts/network.js
@@ -3,13 +3,12 @@
 
 var form = document.getElementById('module')
 var input = document.getElementsByClassName('module_input')
-//
-// var gene = document.getElementById("gene")
-// var gene_input = document.getElementById("gene_input")
+
+var gene = document.getElementById("gene")
+var gene_input = document.getElementById("gene_input")
 
 
 async function getApi(idOrName){
-  console.log(!(idOrName=='/api/module/'))
   if(idOrName && (!(idOrName == '/api/module/'))){
     const response = await fetch(idOrName);
     const json = await response.json();
@@ -28,55 +27,64 @@ function iniCy(json){
     elements: json,
     style: [ // the stylesheet for the graph
       {
+        selector: 'node',
+        style:{
+          'background-color': '#666',
+        }
+      },{
         selector: 'node[[degree <= 1]]',
         style: {
           'height': 3,
           'width': 3,
-          'background-color': '#666',
           // 'label': 'data(id)'
         }
-      },
-      {
+      },{
         selector: 'node[[degree >= 2]][[degree <= 9]]',
         style: {
           'height': 10,
           'width': 10,
-          'background-color': '#666',
           // 'label': 'data(id)'
         }
-      },
-      {
+      },{
         selector: 'node[[degree >= 10]]',
         style: {
           'height': 13,
           'width': 13,
-          'background-color': '#666',
           // 'label': 'data(id)'
         }
-      },
-
-      {
+      },{
         selector: 'edge',
         style: {
           'width': 1,
-          'line-color': '#ccf',
+          'line-color': '#88A7CA',
           'curve-style':'straight',
         }
-      },
-
-      {
+      }, {
       selector: 'edge[?directionality]',
       style : {
-        'target-arrow-color': '#ccf',
+        'target-arrow-color': '#88A7CA',
         'target-arrow-shape': 'vee'
       }
-    }
+    },{
+      selector: 'node:selected',
+      style: {
+        'background-color': '#A3CA88',
+        'label':'data(name)',
+        'color': '#fff',
+        'text-outline-color': '#888',
+        'text-outline-width': 3,
+      },
+    },
     ],
 
     layout: {
       name: 'cose-bilkent',
       animate : 'end'
     }
+  });
+  cy.on('tap', 'node', function(evt){
+    var node = evt.target;
+    console.log( 'tapped ' + node.id() );
   });
 }
 form.addEventListener('submit',function(e){
