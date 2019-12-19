735628f93c9264ef26831616a4d078fad2628c50
Jacob Karlström <jacob.karlstrom@gmail.com>
2019-12-19 02:47:20 +0100

    Added new endpoints for typeahead

--- server/Controler/networkdata.js
+++ server/Controler/networkdata.js
@@ -1,5 +1,7 @@
-var model = require('../Model/data')
+var model = require('../model/data')
 var bodyParser = require("body-parser");
+
+
 
 function getNetwork(req, res){
   res.json(model.getNetwork());
@@ -13,7 +15,18 @@ function getSingleGene(req, res){
   res.json(model.getSingleGene(req.query.name));
 }
 
+function getOnlyModules(req, res){
+  res.json(model.getOnlyModules());
+}
+
+function getOnlyGenes(req, res){
+  res.json(model.getOnlyGenes());
+}
+
+
 
 module.exports.getSingleGene = getSingleGene;
 module.exports.getNetwork = getNetwork;
 module.exports.getModule = getModule;
+module.exports.getOnlyModules = getOnlyModules;
+module.exports.getOnlyGenes = getOnlyGenes;
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,11 +4,15 @@ var controller = require('../Controler/networkdata.js')
 var bodyParser = require("body-parser");
 var cors = require("corser");
 
-console.log(controller.getNetwork, controller.getModule)
+// console.log(controller.getNetwork, controller.getModule, controller.getOnlyModules)
 
 router.get('/', controller.getNetwork);
 
 router.get('/module/:module_id', controller.getModule)
 
 router.get('/gene', controller.getSingleGene)
+
+router.get('/modules', controller.getOnlyModules)
+
+router.get('/genes', controller.getOnlyGenes)
 module.exports = router;
